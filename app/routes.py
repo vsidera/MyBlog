aaa6ed3e7b10c8eb1dbb90cbf4e42b4cd4694183
vsidera <siderravictor@gmail.com>
2020-09-25 19:13:59 +0300

    add login and registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,29 @@
-from flask import render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from .forms import RegistrationForm, LoginForm
 from app import app
+
+app.config['SECRET_KEY'] = '36a2b76e543b4e98f4f495c4'
 
 @app.route('/')
 def home():
     return render_template ('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)    
